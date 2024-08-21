4fe61f39788b59f854283042088fe28f6a1a4157
Pedro Galembeck <galembeckpedro@gmail.com>
2024-08-21 20:17:01 -0300

    refactor: optimize homepage layout

--- src/app/(marketing)/page.tsx
+++ src/app/(marketing)/page.tsx
@@ -3,17 +3,23 @@ import Link from "next/link";
 import Image from "next/image";
 
 import { Wrapper, Container, Icons } from "@/components";
+import Marquee from "@/components/ui/marquee";
+import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import SectionBadge from "@/components/ui/section-badge";
 import { BorderBeam } from "@/components/ui/border-beam";
+import { LampContainer } from "@/components/ui/lamp";
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
 
-import { features, perks, pricingCards } from "@/constants";
 import { cn } from "@/lib/utils";
+import { features, perks, pricingCards, reviews } from "@/constants";
 
-import { ArrowRight, ChevronRight, Zap } from "lucide-react";
+import { ArrowRight, ChevronRight, UserIcon, Zap } from "lucide-react";
 
 export default function HomePage() {
+    const firstRow = reviews.slice(0, reviews.length / 2);
+    const secondRow = reviews.slice(reviews.length / 2);
+
     return (
         <section className="w-full relative flex flex-col items-center justify-center px-4 md:px-0 py-8">
 
@@ -209,7 +215,127 @@ export default function HomePage() {
             </Wrapper>
 
             {/* Testimonials */}
-            
+            <Wrapper className="flex flex-col items-center justify-center py-12 relative">
+                <div className="hidden md:block absolute -top-1/4 -left-1/3 w-72 h-72 bg-indigo-500 rounded-full blur-[10rem] -z-10"></div>
+                <Container>
+                    <div className="max-w-md mx-auto text-start md:text-center">
+                        <SectionBadge title="Our Customers" />
+                        <h2 className="text-3xl lg:text-4xl font-semibold mt-6">
+                            What people are saying
+                        </h2>
+                        <p className="text-muted-foreground mt-6">
+                            See how Astra empowers businesses of all sizes. Here&apos;s what real people are saying on Twitter
+                        </p>
+                    </div>
+                </Container>
+                <Container>
+                    <div className="py-10 md:py-20 w-full">
+                        <div className="relative flex h-full w-full flex-col items-center justify-center overflow-hidden py-10">
+                            <Marquee pauseOnHover className="[--duration:20s] select-none">
+                                {firstRow.map((review) => (
+                                    <figure
+                                        key={review.name}
+                                        className={cn(
+                                            "relative w-64 cursor-pointer overflow-hidden rounded-xl border p-4",
+                                            "border-zinc-50/[.1] bg-background over:bg-zinc-50/[.15]",
+                                        )}
+                                    >
+                                        <div className="flex flex-row items-center gap-2">
+                                            <UserIcon className="w-6 h-6" />
+                                            <div className="flex flex-col">
+                                                <figcaption className="text-sm font-medium">
+                                                    {review.name}
+                                                </figcaption>
+                                                <p className="text-xs font-medium text-muted-foreground">{review.username}</p>
+                                            </div>
+                                        </div>
+                                        <blockquote className="mt-2 text-sm">{review.body}</blockquote>
+                                    </figure>
+                                ))}
+                            </Marquee>
+                            <Marquee reverse pauseOnHover className="[--duration:20s] select-none">
+                                {secondRow.map((review) => (
+                                    <figure
+                                        key={review.name}
+                                        className={cn(
+                                            "relative w-64 cursor-pointer overflow-hidden rounded-xl border p-4",
+                                            "border-zinc-50/[.1] bg-background over:bg-zinc-50/[.15]",
+                                        )}
+                                    >
+                                        <div className="flex flex-row items-center gap-2">
+                                            <UserIcon className="w-6 h-6" />
+                                            <div className="flex flex-col">
+                                                <figcaption className="text-sm font-medium">
+                                                    {review.name}
+                                                </figcaption>
+                                                <p className="text-xs font-medium text-muted-foreground">{review.username}</p>
+                                            </div>
+                                        </div>
+                                        <blockquote className="mt-2 text-sm">{review.body}</blockquote>
+                                    </figure>
+                                ))}
+                            </Marquee>
+                            <div className="pointer-events-none absolute inset-y-0 left-0 w-1/3 bg-gradient-to-r from-background"></div>
+                            <div className="pointer-events-none absolute inset-y-0 right-0 w-1/3 bg-gradient-to-l from-background"></div>
+                        </div>
+                    </div>
+                </Container>
+            </Wrapper>
+
+            {/* Newsletter */}
+            <Wrapper className="flex flex-col items-center justify-center relative">
+                <Container>
+                    <LampContainer>
+                        <div className="flex flex-col items-center justify-center relative w-full text-center">
+                            <h2 className="text-4xl lg:text-5xl xl:text-6xl lg:!leading-snug font-semibold mt-8">
+                                From Idea to Launch <br /> Faster Than Ever
+                            </h2>
+                            <p className="text-muted-foreground mt-6 max-w-md mx-auto">
+                                Build stunning websites with Astra&apos;s intuitive drag-and-drop builder and powerful AI assistant
+                            </p>
+                            <Button variant="white" className="mt-6" asChild>
+                                <Link href="/sign-in">
+                                    Get started for free
+                                    <ArrowRight className="w-4 h-4 ml-2" />
+                                </Link>
+                            </Button>
+                        </div>
+                    </LampContainer>
+                </Container>
+                <Container className="relative z-[999999]">
+                    <div className="flex items-center justify-center w-full -mt-40">
+                        <div className="flex flex-col md:flex-row items-start md:items-center justify-start md:justify-between w-full px-4 md:px-8 rounded-lg lg:rounded-2xl border border-border/80 py-4 md:py-8">
+                            <div className="flex flex-col items-start gap-4 w-full">
+                                <h4 className="text-xl md:text-2xl font-semibold">
+                                    Join our newsletter
+                                </h4>
+                                <p className="text-base text-muted-foreground">
+                                    Be up to date with everything about AI builder
+                                </p>
+                            </div>
+                            <div className="flex flex-col items-start gap-2 md:min-w-80 mt-5 md:mt-0 w-full md:w-max">
+                                <form action="#" className="flex flex-col md:flex-row items-center gap-2 w-full md:max-w-xs">
+                                    <Input
+                                        required
+                                        type="email"
+                                        placeholder="Enter your email"
+                                        className="focus-visible:ring-0 focus-visible:ring-transparent focus-visible:border-primary duration-300 w-full"
+                                    />
+                                    <Button type="submit" size="sm" variant="secondary" className="w-full md:w-max">
+                                        Subscribe
+                                    </Button>
+                                </form>
+                                <p className="text-xs text-muted-foreground">
+                                    By subscribing you agree with our{" "}
+                                    <Link href="#">
+                                        Privacy Policy
+                                    </Link>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </Container>
+            </Wrapper>
         </section>
     );
 }
