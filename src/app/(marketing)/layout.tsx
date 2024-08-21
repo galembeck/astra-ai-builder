6133d92406bd2232c9ccc9e97f84e71bbe3d485a
Pedro Galembeck <galembeckpedro@gmail.com>
2024-08-21 20:53:20 -0300

    refactor: add footer component to marketing layout

--- src/app/(marketing)/layout.tsx
+++ src/app/(marketing)/layout.tsx
@@ -1,5 +1,6 @@
-import { Navbar } from '@/components';
-import React from 'react'
+import React from 'react';
+
+import { Footer, Navbar } from '@/components';
 
 interface Props {
     children: React.ReactNode;
@@ -10,6 +11,7 @@ const MarketingLayout = ({ children }: Props) => {
         <div className="flex flex-col items-center w-full">
             <Navbar />
             {children}
+            <Footer />
         </div>
     )
 };
