528418f4d0f3817251223d1434032847a312e94f
Pedro Galembeck <galembeckpedro@gmail.com>
2024-08-21 20:53:33 -0300

    feat: add footer component

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,10 +3,12 @@ import { Wrapper } from "./global/wrapper";
 import { Container } from "./global/container";
 
 import { Navbar } from "./navigation/navbar";
+import { Footer } from "./navigation/footer";
 
 export {
     Icons,
     Navbar,
     Wrapper,
     Container,
+    Footer,
 }
