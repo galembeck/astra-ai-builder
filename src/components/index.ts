8239413edf4349d484b5beee68ff86d9c56fe3fd
Pedro Galembeck <galembeckpedro@gmail.com>
2024-08-21 19:04:23 -0300

    refactor: add missing components to index.ts

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,12 @@
 import Icons from "./global/icons";
+import { Wrapper } from "./global/wrapper";
+import { Container } from "./global/container";
+
+import { Navbar } from "./navigation/navbar";
 
 export {
     Icons,
+    Navbar,
+    Wrapper,
+    Container,
 }
